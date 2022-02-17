f3af950da34d61fb31178e01eeddbbf0b5ca5ad0
RobsonM12 <robsonmorais1996@hotmail.com>
2022-02-17 15:34:56 -0300

    Atividades_17-02-2022

---
+++ Blog_Pessoal/Tabela_do_blog.sql
@@ -0,0 +1,8 @@
+use db_blogpessoal;
+insert into tb_postagens(date, texto, titulo) values (current_timestamp(),
+"Blog Pessoal funcionando!", "Minha Primeira postagem");
+
+insert into tb_postagens(date, texto, titulo) values (current_timestamp(),
+"Blog Pessoal funcionando!", "Minha Primeira postagem");
+
+select * from  tb_postagens ;
